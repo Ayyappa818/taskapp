c25836d2c74a4f9cd9dd7d6633d9b22dea86029c
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-06-07 11:38:22 +0530

    Task-2

---
+++ src/Alphabet.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Alphabet() {
+  return (
+    <div>
+    <h1>Alphabet</h1>
+      <br />
+      <h1>
+        A 
+        <p>Apple</p>
+      </h1>
+      <h1>
+        B 
+        <p>Ball</p>
+      </h1>
+      <h1>
+        C 
+        <p>Cat</p>
+      </h1>
+    </div>
+  )
+}
+
+export default Alphabet
